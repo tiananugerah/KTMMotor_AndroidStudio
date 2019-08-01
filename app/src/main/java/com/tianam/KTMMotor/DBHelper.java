e6c1b2a113b0953eb3e11459c7dea03ff5dfb4ac
tiananugerah <32483356+tiananugerah@users.noreply.github.com>
2019-08-01 12:51:17 +0700

    Update

---
+++ app/src/main/java/com/tianam/KTMMotor/DBHelper.java
@@ -0,0 +1,27 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table pengukur(no integer primary key, min text null, avr text null, max text null, real text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO pengukur (min, avr, max, real, alamat) VALUES ('1', '12', '12', '12','12');";
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+    }
+}
