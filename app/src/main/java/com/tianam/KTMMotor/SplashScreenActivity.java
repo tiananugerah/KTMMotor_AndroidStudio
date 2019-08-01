98b3848fe39feec6ca4824bc13f5c47769ecab67
tiananugerah <32483356+tiananugerah@users.noreply.github.com>
2019-08-01 10:59:20 +0700

    Initial commit

---
+++ app/src/main/java/com/tianam/KTMMotor/SplashScreenActivity.java
@@ -0,0 +1,51 @@
+package com.tianam.KTMMotor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by tianam on 2019/7/21.
+ */
+public class SplashScreenActivity extends Activity {
+
+    //Set waktu lama splashscreen
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(SplashScreenActivity.this, SpeedActivity.class);
+                startActivity(i);
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    }
+
+    ;
+
+}
